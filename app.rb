57e7f3d855e0f089fd02eda719d89290fdc41aab
github username <github email address>
2020-06-03 21:32:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,35 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+# end
+  get '/name' do
+    "My name is Something"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Hometown"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Street"        
+  end
 end
+  # get '/names/:id' do
+  #   @name = Name.find(params[:id])
+
+  #   erb ":names/index.html.erb"
+  #   "My name is <%= #{@name} %>"
+  # end
+  # get '/hometowns' do
+  #   @hometowns = Hometown.all
+
+  #   erb ":hometowns/index.html.erb"
+  #   "My hometown is __"
+  # end
+  # get '/favorite-songs' do
+  #   @favorite-songs = Name.all
+
+  #   erb ":favorite-songs/index.html.erb"
+  #   "My favorite song is __"
+  # end
